1589eafba615e6e752e96a129db38b902ef6e118
De Jesus Oliveira Armand <Armand@localhost.localdomain>
2017-12-18 16:57:46 +0100

    Disp

--- disp_picture.c
+++ disp_picture.c
@@ -6,18 +6,24 @@ int main(void)
 	sfVideoMode mode = {800, 600, 32};
 	sfRenderWindow* window;
 	sfEvent event;
-	sfImage* image;
+	sfTexture* texture;
+	sfSprite* sprite;
 
 	window = sfRenderWindow_create(mode, "SFML window", sfResize | sfClose, NULL);
 	if (!window)
 		return (84);
-	image = sfImage_createFromPixels(10, 50, NULL);
+	texture = sfTexture_createFromFile("hogwarts.jpg", NULL);
+	if (!texture)
+		return (84);
+	sprite = sfSprite_create();
+	sfSprite_setTexture(sprite, texture, sfTrue);
 	while (sfRenderWindow_isOpen(window)) {
 		while (sfRenderWindow_pollEvent(window, &event)) {
 			if (event.type == sfEvtClosed)
 				sfRenderWindow_close(window);
 		}
 		sfRenderWindow_clear(window, sfBlack);
+		sfRenderWindow_drawSprite(window, sprite, NULL);
 	}
 	return (0);
 }
